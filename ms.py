7d49b1beed33d9126a01446f0d1f804b2d141e26
Maximilian Stock <Maximilian@troja2.local>
2019-10-15 17:37:31 +0200

    20191015

--- ms.py
+++ ms.py
@@ -5,14 +5,18 @@ import os
 # SYSTEM PARAMETERS
 count_params = len(sys.argv)-1
 
-if count_params == 0:
-    print("no script found")
-else:
+if count_params > 0:
     module = sys.argv[1]
 
     if module == 'react':
         import react.module
+
+    if module == 'redux':
+        import redux.module
         
     else:
         print(module+" not found")
+        
+else:
+    print("no script found")
     
--- redux/module.py
+++ redux/module.py
@@ -11,21 +11,53 @@ if count_params > 1:
 
     if count_params > 2:
 
-        if function == 'create-app':
-            app_name = sys.argv[3]
+        if function == 'create-reducer':
+            reducerFileName = sys.argv[3]
 
-            os.system('npx create-react-app '+app_name)
+            sep = ""
+            reducerName = sep.join(list(map(lambda text: text.capitalize(), reducerFileName.split('-'))))
+            templatesFolder = os.path.dirname(os.path.realpath(__file__))+'\\templates\\'
 
-            os.chdir("./"+app_name)
-            os.system('npm i redux')
-            os.system('npm i react-redux')
-            # os.system('npm i react-router-dom')
-            os.system('npm i node-sass')
+            os.chdir("./src/redux")
+            os.mkdir(reducerName)
+            os.chdir(reducerName)
 
-            os.chdir("./src")
-            os.mkdir("components")
-            os.mkdir("pages")
-            os.mkdir("redux")
+            f = open(templatesFolder+"reducer.txt", "r")
+            reducerTemplate = f.read()
+            f.close()
+
+            f = open(templatesFolder+"actions.txt", "r")
+            actionsTemplate = f.read()
+            f.close()
+
+            f = open(templatesFolder+"types.txt", "r")
+            typesTemplate = f.read()
+            f.close()
+
+            reducerTemplate = reducerTemplate.replace('%-REDUCER_FILE_NAME-%', reducerFileName)
+            reducerTemplate = reducerTemplate.replace('%-REDUCER_NAME-%', reducerName)
+
+            actionsTemplate = actionsTemplate.replace('%-REDUCER_FILE_NAME-%', reducerFileName)
+
+
+            f = open(reducerFileName+".reducer.js", "a")
+            f.write(reducerTemplate)
+            f.close()
+
+            f = open(reducerFileName+".actions.js", "a")
+            f.write(actionsTemplate)
+            f.close()
+
+            f = open(reducerFileName+".types.js", "a")
+            f.write(typesTemplate)
+            f.close()
+
+            f = open(reducerFileName+".utils.js", "a")
+            f.write("")
+            f.close()
+
+            print('redux reducer '+reducerFileName+' created successfully!')
+
 
         elif function == 'create-fun-comp':
             name = sys.argv[3]
